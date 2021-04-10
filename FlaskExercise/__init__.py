ba31adc3a9c6c3509650b39fc0cfc3e82fc97472
Josh Haines <Josh@JoshHaines.com>
2021-04-09 23:38:04 -0400

    working

--- FlaskExercise/__init__.py
+++ FlaskExercise/__init__.py
@@ -1,5 +1,21 @@
-import logging
+from logging.config import dictConfig
 from flask import Flask
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 wsgi_app = app.wsgi_app
--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -7,6 +7,17 @@ def home():
     log = request.values.get('log_button')
     # TODO: Appropriately log the different button presses
     #   with the appropriate log level.
+    if log == 'info':
+        app.logger.info('info Log Test')
+    elif log == 'warning':
+        app.logger.warning('warning Log Test')
+    elif log == 'error':
+        app.logger.error('error Log Test')
+    elif log == 'critical':
+        app.logger.critical('critical Log Test')
+    else:
+        pass
+    
     return render_template(
         'index.html',
         log=log
